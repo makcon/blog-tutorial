c2675c22eb147dd53fd2ce996f498d74ed93ab6a
makcon <makcon2003@mail.ru>
2013-11-05 00:26:25 +0400

    add users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   attr_accessible :text, :title, :user_id
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
+  has_many :posts
+
   validates :name, length: {minimum: 5, maximum: 20}
 end
