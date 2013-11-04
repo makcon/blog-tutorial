26fd2ad0ee4de860542e07113bdb46945f4e6a67
makcon <makcon2003@mail.ru>
2013-11-04 14:05:02 +0400

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    #render text: params[:post].inspect
+    @post = Post.new(params[:post])
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  #private
+  #def post_params
+  #  params.require(:post).permit(:title, :text)
+  #end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '8d39117fafe45d4b52914a7de0dccef0a98584727a8ce6ee9b9eb77e767a1d274ede56c2761a805df140746b6b0022d36a157859ade178991cf0413c358c6996'
