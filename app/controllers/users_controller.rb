c88efd1f2eaff22a3f5834815be48b5c69a2e2bd
makcon <makcon2003@mail.ru>
2013-11-17 16:56:56 +0400

    add singup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,19 +37,31 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
+  ## POST /users
+  ## POST /users.json
+  #def create
+  #  @user = User.new(params[:user])
+  #
+  #  respond_to do |format|
+  #    if @user.save
+  #      #flash[:success] = "Welcome to the Sample App!"
+  #      #redirect_to @user
+  #      format.html { redirect_to @user, success: 'User was successfully created.' }
+  #      #format.json { render json: @user, status: :created, location: @user }
+  #    else
+  #      format.html { render action: "new" }
+  #      format.json { render json: @user.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
+
   def create
     @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
     end
   end
 
